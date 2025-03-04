0650f1189c72c46ef90e6f6b44f4f2038538a128
751775098 <751775098@qq.com>
2025-03-04 22:44:01 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,19 +9,19 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeComponents from "rehype-components"; /* Render the custom directive content */
 import rehypeKatex from "rehype-katex";
 import rehypeSlug from "rehype-slug";
-import remarkDirective from "remark-directive"; /* Handle directives */
-import remarkGithubAdmonitionsToDirectives from "remark-github-admonitions-to-directives";
+import remarkDirective from "remark-directive"; /* Handle directives */  
+import remarkGithubAdmonitionsToDirectives from "remark-github-admonitions-to-directives";  
 import remarkMath from "remark-math";
 import remarkSectionize from "remark-sectionize";
 import { AdmonitionComponent } from "./src/plugins/rehype-component-admonition.mjs";
 import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.mjs";
-import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
-import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
+import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";  
+import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";  
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "http://ioioi.work.gd/",          
   base: "/",
   trailingSlash: "always",
   integrations: [
